019d47fb799c6658240b04537dd7f05a1e8cb574
Nadjem-47 <nadgmo@gmail.com>
2026-03-08 20:28:32 +0100

    Add named function

--- com/craftinginterpreters/lox/Expr.java
+++ com/craftinginterpreters/lox/Expr.java
@@ -11,6 +11,7 @@ abstract class Expr {
         R visitLogicalExpr(Logical expr);
         R visitUnaryExpr(Unary expr);
         R visitVariableExpr(Variable expr);
+        R visitCallExpr(Call expr);
         R visitConditionalExpr(Conditional expr);
         R visitCommaExpr(Comma expr);
     }
@@ -115,6 +116,23 @@ abstract class Expr {
         @Override
         <R> R accept(Visitor<R> visitor) {
             return visitor.visitVariableExpr(this);
+        }
+    }
+
+    static class Call extends Expr {
+        final Expr callee;
+        final Token paren;
+        final List<Expr> arguments;
+
+        Call(Expr callee, Token paren, List<Expr> arguments) {
+            this.callee = callee;
+            this.paren = paren;
+            this.arguments = arguments;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitCallExpr(this);
         }
     }
 
